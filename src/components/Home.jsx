afbd670f4b603ddd760410c51763059a59987607
anayagarde <anayagarde@gmail.com>
2023-04-17 02:37:56 +0530

    Added home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import image from "../assets/img.jpeg"
+import { MdKeyboardDoubleArrowRight } from 'react-icons/md'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
+        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+            <div className='flex flex-col justify-center h-full'>
+                <h2 className='text-4xl md:text-7xl font-bold text-white'> I'm a Full Stack Developer</h2>
+                <p className='text-gray-500 py-4 max-w-md'>
+                    Hi, I am Anaya Garde. I am currently working as a Software Developer. I enjoy working on Full Stack Applications and exploring new Technologies.
+                </p>
+                <div>
+                    <button className='group text-white w-fit px-6 py-6 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                        Portfolio
+                        <span className='group-hover:rotate-90 duration-300'>
+                            <MdKeyboardDoubleArrowRight size={20} className='ml-1'/>
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div>
+                <img src={image} className="rounded-2xl mx-auto w-1/2 md:-w-full"></img>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
         <div>
-            <h1 className='text-5xl font-signature ml-2'>Anaya</h1>
+            <h1 className='lg:text-5xl text-3xl font-signature ml-2'>Anaya Garde</h1>
         </div>
         <ul className='hidden md:flex'>
             {links.map((r) => (
