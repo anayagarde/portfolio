d6b9aed6f835d97a6b54c9dd4f6a3800c99e0560
anayagarde <anayagarde@gmail.com>
2023-04-18 02:28:30 +0530

    Created boilerplate pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 
 function App() {
@@ -10,7 +14,11 @@ function App() {
       <Navbar/>
       <Home/>
       <About />
+      <Education />
+      <Experience />
       <Projects />
+      <Portfolio/>
+      <Contact/>
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div name="contact" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
+            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center w-full h-full px-4'>
+                <h2 className='pb-4 text-4xl font-bold inline border-b-4 text-white'>
+                    Contact
+                </h2>
+               
+            </div>
+        </div>
+  )
+}
+
+export default Contact
