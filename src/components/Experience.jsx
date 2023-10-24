f69046714bb53632afd1ab2fc8fb3af7df38a427
anayagarde <anayagarde@gmail.com>
2023-10-25 03:41:07 +0530

    added experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,19 +1,45 @@
 import React from 'react'
+import { MdWorkOutline } from 'react-icons/md'
+import { IoIosCodeWorking } from 'react-icons/io'
+import { PiProjectorScreenBold } from 'react-icons/pi'
 
 const Experience = () => {
   return (
     <div name="experience" className='h-screen w-full bg-gradient-to-b from-slate-300 to-slate-50 dark:bg-gradient-to-b dark:from-slate-900 dark:via-slate-800 dark:to-slate-600'>
             <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center w-full h-full px-4'>
                 <h2 className='pb-4 text-4xl font-bold inline border-b-4 border-zinc-800 dark:border-zinc-50 text-gray-700 dark:text-white'>
-                    Experience
+                    Work Experience
                 </h2>
-                <p className='pt-8 flex text-gray-700 dark:text-white'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-                <br/>
-                <p className='pb-6 text-gray-700 dark:text-white'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+                <div className='z-0 w-full py-16 px-8 flex justify-items-center'>
+                    <ol class="relative border-l border-gray-600 dark:border-gray-300">                  
+                        <li class="mb-10 ml-6">            
+                            <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-500">
+                            <MdWorkOutline />
+                            </span>
+                            <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">Deutsche India Private Ltd</h3>
+                            <time class="block mb-2 text-sm font-normal leading-none text-gray-700 dark:text-gray-400">Graduate Analyst (July 2022 - July 2023)</time>
+                            <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Built microservices using Java, Spring, Axiom, VAS, GraphQL, and Oracle.</p>
+                            <time class="block mb-2 text-sm font-normal leading-none text-gray-700 dark:text-gray-400">Senior Analyst (July 2023 - Present)</time>
+                            <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Working as full stack developer in Corporate Bank Technology division's Working Capital team.</p>
+                        </li>
+                        <li class="mb-10 ml-6">
+                            <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-500">
+                            <IoIosCodeWorking />
+                            </span>
+                            <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Deutsche India Private Ltd</h3>
+                            <time class="block mb-2 text-sm font-normal leading-none text-gray-700 dark:text-gray-400">Technology Intern (May 2021 - July 2021)</time>
+                            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Worked on creating a full stack Data Visualization Application using ReactJS, Spring Boot, and MySQL.</p>
+                        </li>
+                        <li class="ml-6">
+                            <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-500">
+                            <PiProjectorScreenBold />
+                            </span>
+                            <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Veritas Technologies</h3>
+                            <time class="block mb-2 text-sm font-normal leading-none text-gray-700 dark:text-gray-400">Project Intern (October 21 - June 2022)</time>
+                            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Worked on Final Year project on Serverless Data Protection on Cloud</p>
+                        </li>
+                    </ol>
+                </div>
             </div>
         </div>
   )
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,21 +1,35 @@
 import React from 'react'
 
 const Portfolio = () => {
+
   return (
     <div name="portfolio" className='h-screen w-full bg-gradient-to-b from-slate-300 to-slate-50 dark:bg-gradient-to-b dark:from-slate-900 dark:via-slate-800 dark:to-slate-600'>
             <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center w-full h-full px-4'>
                 <h2 className='pb-4 text-4xl font-bold inline border-b-4 border-zinc-800 dark:border-zinc-50 text-gray-700 dark:text-white'>
                     Portfolio
                 </h2>
-                <p className='pt-8 flex text-gray-700 dark:text-white'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-                <br/>
-                <p className='pb-6 text-gray-700 dark:text-white'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+              <div class="border-b border-gray-200 dark:border-gray-700 py-16">
+                  <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+                      <li class="mr-2">
+                          <button class="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group">
+                             Achievements
+                          </button>
+                      </li>
+                      <li class="mr-2">
+                          <button class="inline-flex items-center justify-center p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500 group" aria-current="page">
+                              Co-curricular Activities
+                          </button>
+                      </li>
+                      <li class="mr-2">
+                          <button class="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group">
+                              Extra Curricular Activities
+                          </button>
+                      </li>
+                  </ul>
+              </div>
+
+                </div>
             </div>
-        </div>
   )
 }
 
