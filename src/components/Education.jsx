9c73a8a47f5cee7c1c31ca4ac2565bc2fafb3d83
anayagarde <anayagarde@gmail.com>
2023-10-25 11:45:56 +0530

    fixes in design

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -10,30 +10,30 @@ const Education = () => {
                 <h2 className='pb-4 text-4xl font-bold inline border-b-4 border-zinc-800 dark:border-zinc-50 text-gray-700 dark:text-white'>
                     Education
                 </h2>
-                <div className='z-0 w-full py-16 px-8 flex justify-items-center'>
-                    <ol class="relative border-l border-gray-600 dark:border-gray-300">                  
+                <div className='z-0 py-16 px-8 flex justify-items-center'>
+                    <ol class="relative w-full items-center border-l border-gray-600 dark:border-gray-300">                  
                         <li class="mb-10 ml-6">            
                             <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-500">
                             <GrCloudComputer/>
                             </span>
-                            <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">MKSSS's Cummins College of Engineering for Women, Pune</h3>
-                            <time class="block mb-2 text-sm font-normal leading-none text-gray-700 dark:text-gray-400">2018-2022 | CGPA - 9.39</time>
+                            <h3 class="flex items-center mb-1 text-lg font-extrabold text-gray-900 dark:text-white">MKSSS's Cummins College of Engineering for Women, Pune</h3>
+                            <time class="block mb-2 text-sm font-bold leading-none text-gray-700 dark:text-gray-300">2018-2022 | CGPA - 9.39</time>
                             <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Bachelor of Technology in Computer Engineering</p>
                         </li>
                         <li class="mb-10 ml-6">
                             <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-500">
                             <MdOutlineCastForEducation />
                             </span>
-                            <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Dr. Kamladi Shamarao Junior College, Pune</h3>
-                            <time class="block mb-2 text-sm font-normal leading-none text-gray-700 dark:text-gray-400">2016-2018 | 89.9%</time>
+                            <h3 class="mb-1 text-lg font-extrabold text-gray-900 dark:text-white">Dr. Kalmadi Shamarao Junior College, Pune</h3>
+                            <time class="block mb-2 text-sm font-bold leading-none text-gray-700 dark:text-gray-300">2016-2018 | 88.9%</time>
                             <p class="text-base font-normal text-gray-500 dark:text-gray-400">Higher Secondary Education - HSC Board</p>
                         </li>
                         <li class="ml-6">
                             <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-500">
                             <PiBookOpenTextDuotone />
                             </span>
-                            <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">City International School, Kothrud, Pune</h3>
-                            <time class="block mb-2 text-sm font-normal leading-none text-gray-700 dark:text-gray-400">2016 | CGPA - 10</time>
+                            <h3 class="mb-1 text-lg font-extrabold text-gray-900 dark:text-white">City International School, Kothrud, Pune</h3>
+                            <time class="block mb-2 text-sm font-bold leading-none text-gray-700 dark:text-gray-300">2016 | CGPA - 10</time>
                             <p class="text-base font-normal text-gray-500 dark:text-gray-400">Secondary Education - CBSE Board</p>
                         </li>
                     </ol>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,32 +10,32 @@ const Experience = () => {
                 <h2 className='pb-4 text-4xl font-bold inline border-b-4 border-zinc-800 dark:border-zinc-50 text-gray-700 dark:text-white'>
                     Work Experience
                 </h2>
-                <div className='z-0 w-full py-16 px-8 flex justify-items-center'>
-                    <ol class="relative border-l border-gray-600 dark:border-gray-300">                  
+                <div className='z-0 py-16 px-8 flex justify-items-center'>
+                    <ol class="relative w-full items-center border-l border-gray-600 dark:border-gray-300">                  
                         <li class="mb-10 ml-6">            
                             <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-500">
                             <MdWorkOutline />
                             </span>
-                            <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">Deutsche India Private Ltd</h3>
-                            <time class="block mb-2 text-sm font-normal leading-none text-gray-700 dark:text-gray-400">Graduate Analyst (July 2022 - July 2023)</time>
+                            <h3 class="flex items-center mb-1 text-lg font-extrabold text-gray-900 dark:text-white">Deutsche India Private Ltd</h3>
+                            <time class="block mb-2 text-sm font-bold leading-none text-gray-700 dark:text-gray-300">Graduate Analyst (July 2022 - July 2023)</time>
                             <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Built microservices using Java, Spring, Axiom, VAS, GraphQL, and Oracle.</p>
-                            <time class="block mb-2 text-sm font-normal leading-none text-gray-700 dark:text-gray-400">Senior Analyst (July 2023 - Present)</time>
+                            <time class="block mb-2 text-sm font-bold leading-none text-gray-700 dark:text-gray-300">Senior Analyst (July 2023 - Present)</time>
                             <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Working as full stack developer in Corporate Bank Technology division's Working Capital team.</p>
                         </li>
                         <li class="mb-10 ml-6">
                             <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-500">
                             <IoIosCodeWorking />
                             </span>
-                            <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Deutsche India Private Ltd</h3>
-                            <time class="block mb-2 text-sm font-normal leading-none text-gray-700 dark:text-gray-400">Technology Intern (May 2021 - July 2021)</time>
+                            <h3 class="mb-1 text-lg font-extrabold text-gray-900 dark:text-white">Deutsche India Private Ltd</h3>
+                            <time class="block mb-2 text-sm font-bold leading-none text-gray-700 dark:text-gray-300">Technology Intern (May 2021 - July 2021)</time>
                             <p class="text-base font-normal text-gray-500 dark:text-gray-400">Worked on creating a full stack Data Visualization Application using ReactJS, Spring Boot, and MySQL.</p>
                         </li>
                         <li class="ml-6">
                             <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-500">
                             <PiProjectorScreenBold />
                             </span>
-                            <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Veritas Technologies</h3>
-                            <time class="block mb-2 text-sm font-normal leading-none text-gray-700 dark:text-gray-400">Project Intern (October 21 - June 2022)</time>
+                            <h3 class="mb-1 text-lg font-extrabold text-gray-900 dark:text-white">Veritas Technologies</h3>
+                            <time class="block mb-2 text-sm font-bold leading-none text-gray-700 dark:text-gray-300">Project Intern (October 2021 - June 2022)</time>
                             <p class="text-base font-normal text-gray-500 dark:text-gray-400">Worked on Final Year project on Serverless Data Protection on Cloud</p>
                         </li>
                     </ol>
